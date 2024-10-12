0cb77ab41338e5618fd4d0791d9579024c28d63d
sheler1988 <93482890+sheler1988@users.noreply.github.com>
2024-10-12 15:32:29 +0330

    1

---
+++ GetInformation/GetInformation/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+//برنامه ای بنویسید که اطلاعاتی مانند نام, سن و قد کاربر را از ورودی دریافت کند و روی صفحه نمایش دهد.
+
+// برای اینکه مقادیر را از ورودی بکیریم ابتدا برای هر کدام یک پیغام مناسب به کاربر نمایش میدهیم و در مرحله بعد یک متغیر ایجاد میکنیم و آن را از کاربر دریافت میکنیم
+
+Console.Write("Please enter your name: ");
+string Name = Console.ReadLine();
+
+Console.Write("Please enter your lastname: ");
+string Lastname = Console.ReadLine();
+
+Console.Write("Please enter your age: ");
+int Age = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter height : ");
+int Height = Convert.ToInt32(Console.ReadLine());
+
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"Your name is: {Name}");
+Console.WriteLine($"Your Lastname is: {Lastname}");
+Console.WriteLine($"Your Age is: {Age}");
+Console.WriteLine($"Your Height is: {Height}");
+
